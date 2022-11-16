de39623fe97220aac793e76d0edab9c336fee65a
Bae Ji Won <qowhxk@naver.com>
2022-11-17 00:10:39 +0900

    [11/15] JSON으로 값을 보내기 위해 데이터 형식을 정하기 위한 클래스 1. Entity에서 데이터를 받아오기 위해 생성자 생성 2. Service에서 기능 구현하고 기능에 따라 값을 추출하여 저장하기 위한 Setter 생성

--- src/main/java/com/mustache/bbs/Domain/Dto/HospitalResponse.java
+++ src/main/java/com/mustache/bbs/Domain/Dto/HospitalResponse.java
@@ -17,4 +17,26 @@ public class HospitalResponse {
     private Integer totalnumberofbeds;
     private String businesstypename;
     private Float totalareasize;
+    private String businessstatuscode;
+
+
+
+    // businessStatuesName을 제외한 생성자 생성
+    // 다른것들은 DB에서 받아서 그대로 json으로 쏘는데 businessStatuesName은 디비에서 숫자를 받아와 Service에서 비교 후
+    // Set을 통해 값을 삽입후 json으로 보내기 때문에
+    public HospitalResponse(Integer id, String roadnameaddress, String hospitalname, Integer patientroomcount, Integer totalnumberofbeds, String businesstypename, Float totalareasize) {
+        this.id = id;
+        this.roadnameaddress = roadnameaddress;
+        this.hospitalname = hospitalname;
+        this.patientroomcount = patientroomcount;
+        this.totalnumberofbeds = totalnumberofbeds;
+        this.businesstypename = businesstypename;
+        this.totalareasize = totalareasize;
+    }
+
+    // DB에서는 int로 값을 받아와서 Service에서 비교 후 문자열로 바꿔줌
+    public void setBusinessstatuscode(String businessstatuscode) {
+        this.businessstatuscode = businessstatuscode;
+    }
+
 }
